fa38f0f4cc03e9a3b74eed0312c07fe7baea1d17
Javran Cheng <Javran.c@gmail.com>
2016-03-12 00:45:19 -0500

    orphan instances

--- src/Vindinium/Api.hs
+++ src/Vindinium/Api.hs
@@ -17,6 +17,7 @@ import Data.Monoid
 
 import Control.Monad
 import Control.Monad.IO.Class
+import Control.Monad.Trans.Resource
 
 startTraining :: Maybe Int -> Maybe Board -> Vindinium State
 startTraining mi mb = do
@@ -59,8 +60,9 @@ request url val = do
                 , responseTimeout = Nothing
                 }
 
-    liftIO $ withManager defaultManagerSettings $ \mgr ->
-        liftM (decodeBody . responseBody) $ httpLbs req mgr
+    liftIO $ do
+        withManager defaultManagerSettings $ \mgr ->
+            (decodeBody . responseBody) <$> httpLbs req mgr
 
   where
     jsonBody = RequestBodyLBS . encode
@@ -73,90 +75,5 @@ request url val = do
         in
             Object (a <> b)
 
-parseBoard :: Int -> String -> Board
-parseBoard s t =
-    Board s $ map parse (chunks t)
-  where
-    chunks []       = []
-    chunks (_:[])   = error "chunks: even chars number"
-    chunks (a:b:xs) = (a, b):chunks xs
 
-    parse (' ', ' ') = FreeTile
-    parse ('#', '#') = WoodTile
-    parse ('@', x)   = HeroTile $ HeroId $ read [x]
-    parse ('[', ']') = TavernTile
-    parse ('$', '-') = MineTile Nothing
-    parse ('$', x)   = MineTile $ Just $ HeroId $ read [x]
-    parse (a, b)     = error $ "parse: unknown tile pattern " ++ (show $ a:b:[])
 
-printTiles :: [Tile] -> Text
-printTiles =
-    foldl (<>) "" . map printTile
-  where
-    printTile FreeTile = "  "
-    printTile WoodTile = "##"
-    printTile (HeroTile (HeroId i)) = "@" <> (pack $ show i)
-    printTile TavernTile = "[]"
-    printTile (MineTile Nothing) = "$-"
-    printTile (MineTile (Just (HeroId i))) = "$" <> (pack $ show i)
-
-instance ToJSON Key where
-    toJSON (Key k) = String k
-
-instance ToJSON Board where
-    toJSON b  = object [ "size"  .= boardSize b
-                       , "tiles" .= (printTiles $ boardTiles b)
-                       ]
-
-instance FromJSON State where
-    parseJSON (Object o) = State <$> o .: "game"
-                                 <*> o .: "hero"
-                                 <*> o .: "token"
-                                 <*> o .: "viewUrl"
-                                 <*> o .: "playUrl"
-    parseJSON _ = mzero
-
-instance FromJSON Game where
-    parseJSON (Object o) = Game <$> o .: "id"
-                                <*> o .: "turn"
-                                <*> o .: "maxTurns"
-                                <*> o .: "heroes"
-                                <*> o .: "board"
-                                <*> o .: "finished"
-    parseJSON _ = mzero
-
-instance FromJSON GameId where
-    parseJSON x = GameId <$> parseJSON x
-
-instance FromJSON Hero where
-    parseJSON (Object o) = Hero <$> o .: "id"
-                                <*> o .: "name"
-                                <*> o .:? "userId"
-                                <*> o .:? "elo"
-                                <*> o .: "pos"
-                                <*> o .: "life"
-                                <*> o .: "gold"
-                                <*> o .: "mineCount"
-                                <*> o .: "spawnPos"
-                                <*> o .: "crashed"
-    parseJSON _ = mzero
-
-instance FromJSON HeroId where
-    parseJSON x = HeroId <$> parseJSON x
-
-instance FromJSON Pos where
-    {-parseJSON (Object o) = Pos <$> o .: "x" <*> o .: "y"-}
-    {-AA 20140204 These seem to be labelled around the wrong way in the JSON-}
-    parseJSON (Object o) = Pos <$> o .: "y" <*> o .: "x"
-    parseJSON _ = mzero
-
-instance FromJSON Board where
-    parseJSON (Object o) = parseBoard <$> o .: "size" <*> o .: "tiles"
-    parseJSON _ = mzero
-
-instance ToJSON Dir where
-    toJSON Stay = String "Stay"
-    toJSON North = String "North"
-    toJSON South = String "South"
-    toJSON East = String "East"
-    toJSON West = String "West"
--- src/Vindinium/Types.hs
+++ src/Vindinium/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, OverloadedStrings #-}
 module Vindinium.Types
         ( Vindinium
         , runVindinium
@@ -18,10 +18,14 @@ module Vindinium.Types
         )
     where
 
+import qualified Data.Text as T
 import Data.Text (Text)
 
 import Control.Monad.Reader (MonadReader, ReaderT, runReaderT, asks)
-import Control.Monad.IO.Class (MonadIO)
+import Data.Aeson
+import Data.Monoid
+import Control.Monad
+import Control.Monad.IO.Class
 
 newtype Key = Key Text deriving (Show, Eq)
 
@@ -93,3 +97,91 @@ data Pos = Pos {
 
 data Dir = Stay | North | South | East | West
     deriving (Show, Eq)
+
+instance ToJSON Key where
+    toJSON (Key k) = String k
+
+instance ToJSON Board where
+    toJSON b  = object [ "size"  .= boardSize b
+                       , "tiles" .= (printTiles $ boardTiles b)
+                       ]
+
+instance FromJSON State where
+    parseJSON (Object o) = State <$> o .: "game"
+                                 <*> o .: "hero"
+                                 <*> o .: "token"
+                                 <*> o .: "viewUrl"
+                                 <*> o .: "playUrl"
+    parseJSON _ = mzero
+
+instance FromJSON Game where
+    parseJSON (Object o) = Game <$> o .: "id"
+                                <*> o .: "turn"
+                                <*> o .: "maxTurns"
+                                <*> o .: "heroes"
+                                <*> o .: "board"
+                                <*> o .: "finished"
+    parseJSON _ = mzero
+
+instance FromJSON GameId where
+    parseJSON x = GameId <$> parseJSON x
+
+instance FromJSON Hero where
+    parseJSON (Object o) = Hero <$> o .: "id"
+                                <*> o .: "name"
+                                <*> o .:? "userId"
+                                <*> o .:? "elo"
+                                <*> o .: "pos"
+                                <*> o .: "life"
+                                <*> o .: "gold"
+                                <*> o .: "mineCount"
+                                <*> o .: "spawnPos"
+                                <*> o .: "crashed"
+    parseJSON _ = mzero
+
+instance FromJSON HeroId where
+    parseJSON x = HeroId <$> parseJSON x
+
+instance FromJSON Pos where
+    {-parseJSON (Object o) = Pos <$> o .: "x" <*> o .: "y"-}
+    {-AA 20140204 These seem to be labelled around the wrong way in the JSON-}
+    parseJSON (Object o) = Pos <$> o .: "y" <*> o .: "x"
+    parseJSON _ = mzero
+
+instance FromJSON Board where
+    parseJSON (Object o) = parseBoard <$> o .: "size" <*> o .: "tiles"
+    parseJSON _ = mzero
+
+instance ToJSON Dir where
+    toJSON Stay = String "Stay"
+    toJSON North = String "North"
+    toJSON South = String "South"
+    toJSON East = String "East"
+    toJSON West = String "West"
+
+parseBoard :: Int -> String -> Board
+parseBoard s t =
+    Board s $ map parse (chunks t)
+  where
+    chunks []       = []
+    chunks (_:[])   = error "chunks: even chars number"
+    chunks (a:b:xs) = (a, b):chunks xs
+
+    parse (' ', ' ') = FreeTile
+    parse ('#', '#') = WoodTile
+    parse ('@', x)   = HeroTile $ HeroId $ read [x]
+    parse ('[', ']') = TavernTile
+    parse ('$', '-') = MineTile Nothing
+    parse ('$', x)   = MineTile $ Just $ HeroId $ read [x]
+    parse (a, b)     = error $ "parse: unknown tile pattern " ++ (show $ a:b:[])
+
+printTiles :: [Tile] -> Text
+printTiles =
+    foldl (<>) "" . map printTile
+  where
+    printTile FreeTile = "  "
+    printTile WoodTile = "##"
+    printTile (HeroTile (HeroId i)) = "@" <> (T.pack $ show i)
+    printTile TavernTile = "[]"
+    printTile (MineTile Nothing) = "$-"
+    printTile (MineTile (Just (HeroId i))) = "$" <> (T.pack $ show i)
