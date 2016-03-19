371148a0effec7215ae51df4c20b3034c5054990
Javran Cheng <Javran.c@gmail.com>
2016-03-18 21:53:58 -0400

    dijkstra's algorithm

---
+++ src/Vindinium/Board/ShortestPath.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE TypeFamilies #-}
+module Vindinium.Board.ShortestPath where
+
+import Vindinium.Types
+import qualified Data.Array as Arr
+import qualified Data.Array.MArray as Arr
+import qualified Data.Array.ST as Arr
+import qualified Data.Map.Strict as Map
+import Data.Function
+import Control.Monad.ST
+import Data.Ix
+import Data.List
+import Data.Maybe
+
+import qualified Data.HashPSQ as PSQ
+
+data PathInfo = PathInfo
+  { piFrom :: Dir
+  , piDist :: Int
+  } deriving (Show)
+
+findShortestPaths :: Board -> (Int, Int) -> Map.Map (Int,Int) (Maybe PathInfo)
+findShortestPaths (b@(Board sz mat)) src = findShortestPaths' initQueue Map.empty
+  where
+    bound = Arr.bounds mat
+    inf = sz * sz * 2
+    expand :: (Int, Int) -> [( (Int,Int), Dir)]
+    expand (x,y) = filter (inRange bound . fst)
+                          [ ((x,y-1), West)
+                          , ((x,y+1), East)
+                          , ((x-1,y), North)
+                          , ((x+1,y), South) ]
+    initQueue = PSQ.insert src 0 (Just (PathInfo Stay 0))
+                $ PSQ.fromList (mapMaybe tr (Arr.assocs mat))
+      where
+        tr (_,WoodTile) = Nothing
+        tr (coord,_) = Just (coord,inf,Nothing)
+    findShortestPaths' :: ( arr ~ Arr.STArray s (Int,Int) (Maybe PathInfo)
+                            -- PSQ <coord> <distance> <path info>
+                          , queue ~ PSQ.HashPSQ (Int,Int) Int (Maybe PathInfo)
+                          )
+                       => queue
+                       -> Map.Map (Int,Int) (Maybe PathInfo)
+                       -> Map.Map (Int,Int) (Maybe PathInfo)
+    findShortestPaths' q visited = case PSQ.minView q of
+        Nothing -> visited
+        Just (coord,dist,v,newQ) ->
+            let newVisited = Map.insert coord v visited
+                -- update pqueue
+                newQ2 = foldl' go newQ (expand coord)
+                go curQueue (curCoord,curDir) =
+                    snd (PSQ.alter modify curCoord curQueue)
+                  where
+                    modify Nothing = ((), Nothing)
+                    modify (e@(Just (curDist,_)))
+                        | atCoord b curCoord == WoodTile = ((), e)
+                        | dist+1 < curDist = ((), Just (dist+1, Just (PathInfo curDir (dist+1))))
+                        | otherwise = ((), e)
+            in findShortestPaths' newQ2 newVisited
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,9 @@
 import Test.Hspec
 import Vindinium.Board
+import Vindinium.Types
+import Vindinium.Board.ShortestPath
+import Control.Monad
+import qualified Data.Map.Strict as Map
 
 boardSample :: Board
 boardSample = parseBoard 14 $ concat
@@ -25,6 +29,16 @@ main = hspec $ do
         it "reads right hero positions" $ do
             (boardSample `atCoord` (3,3)) `shouldBe` HeroTile 1
             (boardSample `atCoord` (10,3)) `shouldBe` HeroTile 2
+            let result = findShortestPaths boardSample (3,3)
+            forM_ [0..13] $ \row -> do
+                forM_ [0..13] $ \col -> do
+                    case Map.lookup (row,col) result of
+                        Nothing -> putStr " "
+                        Just Nothing -> putStr "?"
+                        Just (Just (PathInfo d _)) -> putStr (case d of
+                                                                  Stay -> "+"
+                                                                  _ -> (take 1 . show $ d))
+                putStrLn ""
     -- TODO: get all important object positions
     -- taverns / spawning point / mines / collect all non-blocks for preprocessing map
     -- TODO: shortest path search verification
